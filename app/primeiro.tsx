200d2bc9321f68ebceeeef7772e7df370b1688b4
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-11-02 23:16:27 -0300

    segundo commit

---
+++ app/primeiro.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import { styles } from "@/constants/styles";
+
+const Primeiro = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Primeiro Componente</Text>
+        </View>
+    )
+}
+
+export default Primeiro;
